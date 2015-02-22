c77e51696b78f49a5425a64b9e97e7ac0542256a
JakeChapman <chapmajb@mail.uc.edu>
2015-02-22 12:38:01 -0500

    Button Style changes

--- QuizScript.js
+++ QuizScript.js
@@ -1,7 +1,19 @@
 /**
  * Created by Jake on 2/21/2015.
  */
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '')
+var db = window.openDatabase('db.sqlite', '1.0', 'NclexPrep', 1024 * 1024);
+
 function QuizPageStart()
 {
-    document.getElementById("QuestionText").innerHTML = "This is the damn question space!";
+    db.transaction(function(tx)
+    {
+        tx.executeSql('Select QuestionText From Questions where QuestionID = 1',[],
+        function(tx, results)
+        {
+            document.getElementById("QuestionText").innerText = "Gets Here";//results.rows.item(0).valueOf();
+        })
+    });
 }
+
