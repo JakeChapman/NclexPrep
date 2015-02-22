990cbf7a6ef4f35175efc8b1fe10b26c9082513e
JakeChapman <chapmajb@mail.uc.edu>
2015-02-22 16:28:32 -0500

    Added Database Files

--- QuizScript.js
+++ QuizScript.js
@@ -1,19 +1,53 @@
 /**
  * Created by Jake on 2/21/2015.
  */
-var xhr = new XMLHttpRequest();
-xhr.open('GET', '')
-var db = window.openDatabase('db.sqlite', '1.0', 'NclexPrep', 1024 * 1024);
+var db;
+var version = 1.0;
+var dbName = "NclexPrep";
+var dbDisplayName = "nclex_prep_db";
+var dbSize = 2 * 1024 * 1024;
+try
+{
+    db = openDatabase(dbName, version, dbDisplayName, dbSize, function(database)
+    {
+        alert("database creation callback");
+    });
+}
+catch(e)
+{
+
+}
 
 function QuizPageStart()
 {
-    db.transaction(function(tx)
+    if(db)
     {
-        tx.executeSql('Select QuestionText From Questions where QuestionID = 1',[],
-        function(tx, results)
-        {
-            document.getElementById("QuestionText").innerText = "Gets Here";//results.rows.item(0).valueOf();
-        })
-    });
+        //LoadQuestion();
+
+        //db.transaction(function (tx) {
+        //    tx.executeSql('Select QuestionText From Questions where QuestionID = 1', [], LoadQuestion);
+        //});
+
+    }
 }
 
+//function LoadQuestion()
+//{
+//    db.transaction(function(t)
+//    {
+//        t.executeSql("CREATE TABLE Questions (QuestionID INTEGER PRIMARY KEY, QuestionText TEXT)",
+//            [], function (sqlTransaction, sqlResultSet) {
+//                alert("Table has been created.");
+//            }, function (sqlTransaction, sqlError) {
+//                /* Error handling */
+//            });
+//    });
+//
+//    db.transaction(function(t)
+//    {
+//        "Insert into Questions(QuestionID, QuestionText) Values(1,'This is the Question'"
+//    }, function()
+//    {
+//        alert("SQL statements were executed successfully.");
+//    });
+//}
